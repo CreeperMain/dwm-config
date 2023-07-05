61b93caf0508d9ad1c6847558f52abb870db69cf
CreeperMain <creepermaingaming@gmail.con>
2023-07-06 01:03:40 +0200

    polishing

--- config.def.h
+++ config.def.h
@@ -12,7 +12,10 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 //static const char *fonts[]          = { "Mononoki Nerd Font:size=10" };
-static const char *fonts[]          = {"Mononoki Nerd Font:style:medium:size=12" ,"JetBrainsMono Nerd Font Mono:style:medium:size=19" };
+static const char *fonts[]          = {
+	"mononoki-Regular.otf:style:medium:size=12" ,
+	"MonoidNerdFont-Regular.ttf:pixelsize=16:style=bold" 
+	};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -26,7 +29,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "󱆃", "", "", "󰊗", "", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
