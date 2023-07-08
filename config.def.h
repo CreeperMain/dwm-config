3f1523415e7cd183f5395393909241ac6a1a684a
CreeperMain <creepermaingaming@gmail.con>
2023-07-08 21:11:33 +0200

    added active indicator

--- config.def.h
+++ config.def.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 8;        /* vertical padding for statusbar */
-static const int vertpad            = 12;       /* vertical padding of bar */
+static const int vertpadbar         = 14;        /* vertical padding for statusbar */
+static const int vertpad            = 15;       /* vertical padding of bar */
 static const int sidepad            = 15;       /* horizontal padding of bar */
 static const char *fonts[]          = {
 	"mononoki-Regular.otf:style:medium:size=12" ,
@@ -36,11 +36,6 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "", "","󰟒", "󰆍", "󰯉", "" };
 
-static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
-static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
-static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
-static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
-
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -49,6 +44,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Virt-viewer",  "virt-viewer",      "Virt-viewer",       1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
