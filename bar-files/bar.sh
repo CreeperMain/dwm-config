dab056b92b4201ef19c6cf30377c588c55d09c92
CreeperMain <creepermaingaming@gmail.con>
2023-07-11 13:58:50 +0200

    bar rework

--- bar-files/bar.sh
+++ bar-files/bar.sh
@@ -11,8 +11,8 @@ interval=0
 cpu() {
   cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
 
-  printf "^c$black^ ^b$red^ CPU"
-  printf "^c$white^ ^b$black2^ $cpu_val"
+  printf "^c$black^^b$red^ CPU"
+  printf "^c$white^ ^b$black2^ $cpu_val GHz"
 }
 
 pkg_updates() {
@@ -26,10 +26,40 @@ pkg_updates() {
     printf "  ^c$green^    $updates"" updates"
   fi
 }
-
+#add => cuz it messes up round numbers and look into gruvbox colors
 battery() {
   get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
-  printf "^b$black^ ^c$blue^ 󰁹 $get_capacity"
+  if [ $get_capacity -lt 100 ] && [ $get_capacity -gt 90 ]
+   then
+   printf "^b$black^ ^c$green^ 󰁹 $get_capacity"
+  elif [ $get_capacity -lt 90 ] && [ $get_capacity -gt 80 ]
+   then
+   printf "^b$black^ ^c$green^ 󰂂 $get_capacity"
+  elif [ $get_capacity -lt 80 ] && [ $get_capacity -gt 70 ]
+   then
+   printf "^b$black^ ^c$lgreen^ 󰂁 $get_capacity"
+  elif [ $get_capacity -lt 70 ] && [ $get_capacity -gt 60 ]
+   then
+   printf "^b$black^ ^c$lgreen^ 󰂀 $get_capacity"
+  elif [ $get_capacity -lt 60 ] && [ $get_capacity -gt 50 ]
+   then
+   printf "^b$black^ ^c$yellow^ 󰁿 $get_capacity"
+  elif [ $get_capacity -lt 50 ] && [ $get_capacity -gt 40 ]
+   then
+   printf "^b$black^ ^c$lyellow^ 󰁾 $get_capacity"
+  elif [ $get_capacity -lt 40 ] && [ $get_capacity -gt 30 ]
+   then
+   printf "^b$black^ ^c$orange^ 󰁽 $get_capacity"
+  elif [ $get_capacity -lt 30 ] && [ $get_capacity -gt 20 ]
+   then
+   printf "^b$black^ ^c$lorange^ 󰁼 $get_capacity"
+  elif [ $get_capacity -lt 20 ] && [ $get_capacity -gt 10 ]
+   then
+   printf "^b$black^ ^c$red^ 󰁻 $get_capacity"
+  elif [ $get_capacity -lt 10 ]
+   then
+   printf "^b$black^ ^c$lred^ 󰁺 $get_capacity"
+fi
 }
 
 brightness() {
@@ -43,19 +73,19 @@ mem() {
 }
 
 swap() {
-  printf "^b$blue^^c$black^ SWAP "
+  printf "^b$blue^^c$black^ SWAP"
   printf "^c$white^ ^b$black2^ $(free -h | awk '/^Swap/ { print $3 }' | sed s/i//g)"
 }
 
 wlan() {
 	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^^b$terminalb^Connected" ;;
-	down) printf "^c$black^ ^b$red^ 󰤭 ^d^%s" " ^c$red^^b$terminalb^Disconnected" ;;
+	up) printf "^c$terminalb^ ^b$blue^ 󰤨 ^d^%s" "^c$blue^^b$terminalb^ Connected" ;;
+	down) printf "^c$terminalb^ ^b$red^ 󰤭 ^d^%s" "^c$red^^b$terminalb^ Disconnected" ;;
 	esac
 }
-
+#like wlan but make a module for charging and make it only show when smth is charging
 clock() {
-	printf "^c$black^ ^b$darkblue^ 󱑆 "
+	printf "^c$black^ ^b$darkblue^ 󱑆"
 	printf "^c$black^^b$blue^ $(date '+%H:%M  ')  "
 }
 
@@ -64,5 +94,5 @@ while true; do
   [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
   interval=$((interval + 1))
 
-  sleep 1 && xsetroot -name " $(cpu) $(mem) $(swap) $(battery) $(brightness) $(wlan) $(clock)"
+  sleep 1 && xsetroot -name "$(cpu)$(mem) $(swap) $(battery)% $(brightness) $(wlan)$(clock)"
 done
