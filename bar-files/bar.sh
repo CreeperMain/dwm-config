efaf469ac5f010ae52aae9f3ea850013c38d7174
CreeperMain <creepermaingaming@gmail.com>
2023-08-26 23:09:47 +0200

    backup

--- bar-files/bar.sh
+++ bar-files/bar.sh
@@ -6,7 +6,7 @@
 interval=0
 
 # load colors
-. ~/.config/dwm-config/bar-files/tokyonight 
+. ~/.config/dwm-config/bar-files/tokyo-night 
 
 pkg_updates() {
   #updates=$({ timeout 20 doas xbps-install -un 2>/dev/null || true; } | wc -l) # void
@@ -24,55 +24,55 @@ battery() {
   get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
   if [ $get_capacity -le 100 ] && [ $get_capacity -ge 90 ]
    then
-   printf "^b$black^ ^c$dggreen^ 󰁹 $get_capacity"
+   printf "^b$black2^^c$dggreen^ 󰁹 $get_capacity"
   elif [ $get_capacity -le 90 ] && [ $get_capacity -ge 80 ]
    then
-   printf "^b$black^ ^c$gaqua^ 󰂂 $get_capacity"
+   printf "^b$black2^^c$gaqua^ 󰂂 $get_capacity"
   elif [ $get_capacity -le 80 ] && [ $get_capacity -ge 70 ]
    then
-   printf "^b$black^ ^c$green^ 󰂁 $get_capacity"
+   printf "^b$black2^^c$green^ 󰂁 $get_capacity"
   elif [ $get_capacity -le 70 ] && [ $get_capacity -ge 60 ]
    then
-   printf "^b$black^ ^c$lggreen^ 󰂀 $get_capacity"
+   printf "^b$black2^^c$lggreen^ 󰂀 $get_capacity"
   elif [ $get_capacity -le 60 ] && [ $get_capacity -ge 50 ]
    then
-   printf "^b$black^ ^c$yellow^ 󰁿 $get_capacity"
+   printf "^b$black2^^c$yellow^ 󰁿 $get_capacity"
   elif [ $get_capacity -le 50 ] && [ $get_capacity -ge 40 ]
    then
-   printf "^b$black^ ^c$gyellow^ 󰁾 $get_capacity"
+   printf "^b$black2^^c$gyellow^ 󰁾 $get_capacity"
   elif [ $get_capacity -le 40 ] && [ $get_capacity -ge 30 ]
    then
-   printf "^b$black^ ^c$lgorange^ 󰁽 $get_capacity"
+   printf "^b$black2^^c$lgorange^ 󰁽 $get_capacity"
   elif [ $get_capacity -le 30 ] && [ $get_capacity -ge 20 ]
    then
-   printf "^b$black^ ^c$dgorange^ 󰁼 $get_capacity"
+   printf "^b$black2^^c$dgorange^ 󰁼 $get_capacity"
   elif [ $get_capacity -le 20 ] && [ $get_capacity -ge 10 ]
    then
-   printf "^b$black^ ^c$red^ 󰁻 $get_capacity"
+   printf "^b$black2^^c$red^ 󰁻 $get_capacity"
   elif [ $get_capacity -le 10 ]
    then
-   printf "^b$black^ ^c$gred^ 󰁺 $get_capacity"
+   printf "^b$black2^^c$gred^ 󰁺 $get_capacity"
 fi
 }
 
 wlan() {
 	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-	up) printf "^c$terminalb^^b$blue^ 󰖩 ^d^%s" "^c$blue^^b$terminalb^ Connected " ;;
+	up) printf "^c$terminalb^^b$lblue^ 󰖩 ^d^%s" "^c$lblue^^b$terminalb^ Connected " ;;
 	down) printf "^c$terminalb^^b$red^ 󰖪 ^d^%s" "^c$red^^b$terminalb^ Disconnected " ;;
 	esac
 }
 #like wlan but make a module for charging and make it only show when smth is charging
 clock() {
-	printf "^c$black^^b$darkblue^ 󱑆"
-	printf "^c$black^^b$blue^ $(date '+%H:%M')"
-	printf "^c$black^^b$blue^ 󰨲"
-	printf "^c$black^^b$blue^ $(date "+%d/%m/%Y")"
+	printf "^c$black2^^b$blue^ 󱑆"
+	printf "^c$black2^^b$blue^ $(date '+%H:%M')"
+	printf "^c$black2^^b$blue^ 󰨲"
+	printf "^c$black2^^b$blue^ $(date "+%d/%m/%Y")"
 	printf "     "
 }
 
 audio() {
-	printf "^c$yellow^ 󰋋"
-	printf "^c$yellow^ $(amixer sget Master | awk -F"[][]" '/Left:/ { print $2 }')"
+	printf "^c$yellow^ 󰋋 "
+	printf "^c$yellow^$(amixer sget Master | awk -F"[][]" '/Left:/ {print $2}')%"
 }
 
 charge() {
@@ -94,5 +94,5 @@ while true; do
   [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
   interval=$((interval + 1))
 
-  sleep 1 && xsetroot -name "$(battery)% $(audio)% $(wlan)$(charge)$(kblayout)$(clock)"
+  sleep 1 && xsetroot -name " $(audio)$(battery)% $(charge)$(wlan)$(kblayout)$(clock)"
 done
